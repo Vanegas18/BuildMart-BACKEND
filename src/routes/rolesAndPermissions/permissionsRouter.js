fb2abe68235b559f16d5ffbbad09d3b49b5dd37d
JuanJose <juanjoseevv@gmail.com>
2025-03-13 10:44:14 -0500

    Ajustes routes

--- src/routes/rolesAndPermissions/permissionsRouter.js
+++ src/routes/rolesAndPermissions/permissionsRouter.js
@@ -10,10 +10,10 @@ import { verificarAdmin } from "../../middlewares/auth/configAuth.js";
 
 const router = express.Router();
 
-router.post("/", verificarAdmin, newPermissions);
+router.post("/", newPermissions);
 router.get("/:nombre", verificarAdmin, getPermissionsByName);
 
-router.get("/", verificarAdmin, getPermissions);
+router.get("/", getPermissions);
 router.put("/:nombre", verificarAdmin, updatePermissions);
 router.patch("/:nombre/estado", verificarAdmin, updateStatePermissions);
 
