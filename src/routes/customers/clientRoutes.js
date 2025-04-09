562e5ba28989423435d6635fbd0948d07eda56f6
JuanJose <juanjoseevv@gmail.com>
2025-04-09 16:19:44 -0500

    No pedir validacion al crear un cliente

--- src/routes/customers/clientRoutes.js
+++ src/routes/customers/clientRoutes.js
@@ -1,20 +1,20 @@
-import express from 'express';
-import { 
-    createClient, 
-    getClients, 
-    updateClient 
-} from '../../controllers/customers/clientController.js';
+import express from "express";
+import {
+  createClient,
+  getClients,
+  updateClient,
+} from "../../controllers/customers/clientController.js";
 import { verificarAdmin } from "../../middlewares/auth/configAuth.js";
 
 const router = express.Router();
 
 // Ruta para obtener clientes (con o sin ID)
-router.get('/:id?', verificarAdmin, getClients);
+router.get("/:id?", verificarAdmin, getClients);
 
 // Ruta para crear un cliente
-router.post('/', verificarAdmin, createClient);
+router.post("/", createClient);
 
 // Ruta para actualizar un cliente
-router.put('/:id',verificarAdmin, updateClient);
+router.put("/:id", verificarAdmin, updateClient);
 
 export default router;
